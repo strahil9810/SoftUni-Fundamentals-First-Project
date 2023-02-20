02eec85198026261fc413d199d4d6f910f0c31ed
strahil98 <strahil1998@abv.bg>
2023-02-20 15:53:45 +0200

    Task 1

---
+++ Echo Type.js
@@ -0,0 +1,11 @@
+function echoType(input){
+    console.log(typeof input);
+    
+    if(input === null){
+        console.log('Parameter is not suitable for printing');
+    }
+    else{
+        console.log(input);
+    }
+}
+echoType('Hello, JavaScript!');
