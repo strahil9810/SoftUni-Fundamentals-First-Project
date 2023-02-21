666250feb07974999cc5cc29f1a171ac6602f631
strahil98 <strahil1998@abv.bg>
2023-02-21 11:59:01 +0200

    Task 4

--- Integer and Float.js
+++ Integer and Float.js
@@ -13,3 +13,19 @@ function integerOrFloat(firstNumber, secondNumber, thirdNumber) {
     console.log(`${sum} - ${type}`);
 }
 integerOrFloat(100, 200, 303)
+
+
+// Втори начин за решаване чрез тернарен оператор
+function solve(input){
+    let numberFirst = Number(input.shift());
+    let numberSecond = Number(input.shift());
+    let numberThird = Number(input.shift());
+
+    let sum = numberFirst + numberSecond + numberThird;
+    
+    // Тернарен оператор
+    let typeIntegerOrFloat = Number.isInteger(sum) ? 'Integer' : 'Float';
+
+    console.log(`${sum} - ${typeIntegerOrFloat}`);
+}
+solve([9, 100, 1.1])
