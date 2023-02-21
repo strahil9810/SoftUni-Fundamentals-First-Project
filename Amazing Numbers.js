8145f45ae2706975b2b48e1c0e05f40d19c630f1
strahil98 <strahil1998@abv.bg>
2023-02-21 12:19:43 +0200

    Task 5

---
+++ Amazing Numbers.js
@@ -0,0 +1,15 @@
+function amazingNumber(input){
+    let number = input.toString();
+    let  sum = 0;
+
+    for(let index = 0; index < number.length; index++){
+        let currentNumber = Number(number[index]);
+
+        sum += currentNumber;
+    }
+    
+    let result = sum.toString().includes('9');
+
+    console.log(result ? `${number} Amazing? True` : `${number} Amazing? False`);
+}
+amazingNumber([999])
